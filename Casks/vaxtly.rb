f80616222ceeaf59b113d4509a4d00bf2e91ef9a
vaxtly <vaxtly.app@gmail.com>
2026-02-21 17:29:47 +0000

    Update Vaxtly to v0.2.0

--- Casks/vaxtly.rb
+++ Casks/vaxtly.rb
@@ -1,13 +1,13 @@
 cask "vaxtly" do
-  version "0.1.5"
+  version "0.2.0"
 
   on_arm do
-    sha256 "dc0510f26b209be13f2d7f44ef40aca4bcc01afd780fd83402d4b1de0d734f33"
+    sha256 "93d69aa81fbc5039492100e0db5473d9869886b5738aec7c0da8acf25bffbc42"
     url "https://github.com/vaxtly/app/releases/download/v#{version}/Vaxtly-#{version}-arm64.dmg"
   end
 
   on_intel do
-    sha256 "616032d3308af6b6ad865be2a27d3bf416943610554a65260c17bc4ddf22f01c"
+    sha256 "0db6913fd3fe0fb2d7695906010cfeb799e283f9344ee832f29ae5fcf4b75b2c"
     url "https://github.com/vaxtly/app/releases/download/v#{version}/Vaxtly-#{version}-x64.dmg"
   end
 
